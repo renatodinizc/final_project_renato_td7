4a2a1366e529f5b63869249f1f1b3e049de7de1a
renatodinizc <renatodiniz25@gmail.com>
2021-10-28 15:17:00 -0300

    Correção de erro nas migrations

--- db/migrate/20211018125116_add_proposal_ref_to_chat.rb
+++ db/migrate/20211018125116_add_proposal_ref_to_chat.rb
@@ -1,7 +1,5 @@
 class AddProposalRefToChat < ActiveRecord::Migration[6.1]
   def change
     add_reference :chats, :proposal, null: false, foreign_key: true
-
-    validates :commenter, :message, presence: true
   end
 end
